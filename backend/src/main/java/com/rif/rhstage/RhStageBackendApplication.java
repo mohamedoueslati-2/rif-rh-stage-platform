1df805cf3b4890132f58021c07228a40d8659d0b
mohamed oueslati <mohamedoueslati070@gmail.com>
2026-07-01 18:31:25 +0100

    feat(backend): initialize Spring Boot backend with PostgreSQL connection

---
+++ backend/src/main/java/com/rif/rhstage/RhStageBackendApplication.java
@@ -0,0 +1,22 @@
+package com.rif.rhstage;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhStageBackendApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("../")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+		SpringApplication.run(RhStageBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/rif/rhstage/RhStageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rif.rhstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhStageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
